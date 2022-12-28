3f1607af47099864c2e732a8390b7a16f87a27cc
Jeferson Salazar Diaz <jeferson.salazar@talosdigital.com>
2022-12-28 09:40:42 -0500

    first commit

---
+++ src/test/java/com/springframework/universitycourses/UniversityCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.universitycourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
